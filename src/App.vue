80934ff54119918c86b516a4229c526c99f5fc41
GulDilin <zhenyagurin@gmail.com>
2020-04-26 23:17:40 +0300

    change api url

--- src/App.vue
+++ src/App.vue
@@ -193,7 +193,7 @@ app
     
     methods:{
       get_func() {
-        axios.post('http://127.0.0.1:5000/api/', {
+        axios.post('https://vmat-4-api.herokuapp.com/api/', {
           function: this.func,
           left: this.left,
           right: this.right,
