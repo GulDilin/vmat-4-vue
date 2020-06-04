e3a05c1f5938f655faf939ebae25418c3a6c707f
Guldilin <zhenyagurin@gmail.com>
2020-06-04 16:38:31 +0300

    change api url

--- src/store/SETTINGS.js
+++ src/store/SETTINGS.js
@@ -1,5 +1,5 @@
-// const URL_API = "https://vmat-4-api.herokuapp.com/api/"
-const URL_API = "http://192.168.1.50:8085/api/"
+const URL_API = "https://vmat-4-api.herokuapp.com/api/"
+// const URL_API = "http://192.168.1.50:8085/api/"
 
 export {
     URL_API
